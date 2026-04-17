339ef61995a3fc95a4dbf303ae30dfe947481c5d
Greg Marshall <greg@alley.com>
2026-04-17 14:36:03 -0500

    make rector happy

--- src/features/class-export-post-action.php
+++ src/features/class-export-post-action.php
@@ -20,9 +20,9 @@ class Export_Post_Action implements Feature {
 	 * Boot the feature.
 	 */
 	public function boot(): void {
-		add_filter( 'post_row_actions', [ $this, 'add_export_link' ], 10, 2 );
-		add_filter( 'page_row_actions', [ $this, 'add_export_link' ], 10, 2 );
-		add_action( 'admin_post_export_single_post', [ $this, 'handle_export' ] );
+		add_filter( 'post_row_actions', $this->add_export_link( ... ), 10, 2 );
+		add_filter( 'page_row_actions', $this->add_export_link( ... ), 10, 2 );
+		add_action( 'admin_post_export_single_post', $this->handle_export( ... ) );
 		add_action(
 			'admin_post_nopriv_export_single_post',
 			static function (): void {
